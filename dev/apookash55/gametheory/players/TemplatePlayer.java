58fbc47340cb651ed6106d24a7ac8c358871e966
Apoorv Kashyap <akashyap@gigsky.com>
2024-01-16 14:16:43 +0530

    added template player

---
+++ dev/apookash55/gametheory/players/TemplatePlayer.java
@@ -0,0 +1,14 @@
+package dev.apookash55.gametheory.players;
+
+import dev.apookash55.gametheory.definition.Decision;
+import dev.apookash55.gametheory.definition.Player;
+
+public class TemplatePlayer extends Player {
+    @Override
+    public Decision makeDecision() {
+        /*
+        Add your logic here
+         */
+        return null;
+    }
+}
